41ee947b85640c41c95bc25e75ea884a2ec3e335
RishavGupta1C <91869768+RishavGupta1C@users.noreply.github.com>
2023-09-21 07:56:07 +0530

    second commit Added Grocery_Item and Category

---
+++ lib/models/category.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+enum Categories {
+  fruit,
+  meat,
+  dairy,
+  carbs,
+  sweets,
+  spices,
+  convenience,
+  hygiene,
+  other
+}
+
+class Category {
+  final String title;
+  final Color color;
+
+  const Category({
+    required this.title,
+    required this.color,
+  });
+}
