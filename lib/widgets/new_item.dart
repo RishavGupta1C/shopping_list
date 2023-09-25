2c2567cdcb87f4466932ad771d7284718f799601
RishavGupta1C <91869768+RishavGupta1C@users.noreply.github.com>
2023-09-26 00:07:54 +0530

    twelfth commit sending a post request to backend

--- lib/widgets/new_item.dart
+++ lib/widgets/new_item.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:shopping_list/data/categories.dart';
 import 'package:shopping_list/models/category.dart';
@@ -23,13 +25,27 @@ class _NewItemState extends State<NewItem> {
   void _saveitem() {
     if (_formKey.currentState!.validate()) {
       _formKey.currentState!.save();
-
-      Navigator.of(context).pop(GroceryItem(
-        id: DateTime.now().toString(),
-        name: _enteredName,
-        quantity: _enteredQuantity,
-        category: _selectedCategory,
-      ));
+      final url = Uri.https(
+        'flutter-shopping-list-dacf0-default-rtdb.firebaseio.com',
+        'shopping-list.json',
+      );
+      http.post(
+        url,
+        headers: {
+          'Content-Type': 'application/json', // How the data will be formatted
+        },
+        body: json.encode({
+          'name': _enteredName,
+          'quantity': _enteredQuantity,
+          'category': _selectedCategory.title,
+        }),
+      ); // encode converts data to json-formatted text
+      // Navigator.of(context).pop(GroceryItem(
+      //   id: DateTime.now().toString(),
+      //   name: _enteredName,
+      //   quantity: _enteredQuantity,
+      //   category: _selectedCategory,
+      // ));
     }
   }
 
